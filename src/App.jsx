43bf3f03738350b0c5207bce54310ed67abe7507
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-22 14:01:26 -0600

    04

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ function App() {
 
   return (
     <>
-      
     <header class="py-5 header">
         <div class="container-xl">
             <div class="row justify-content-center justify-content-md-between">
