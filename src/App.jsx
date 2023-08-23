5ead3d70ac25792e0707c28c2a8480f8e32d9263
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-23 12:19:21 -0600

    16

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,10 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map(() => (
+            {data.map((guitar) => (
                 <Guitar 
-                  price={100}
-                  auth={true}
-                  
+                  key={guitar.id}
+                  guitar={guitar}
                 />
             ))}
             
