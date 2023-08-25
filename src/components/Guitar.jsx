97193c03abe921825f513fd602457c17e3e48a28
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-24 19:06:05 -0600

    17

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,6 +2,10 @@ export default function Guitar({guitar}) {
 
     const { id, name, image, description, price } = guitar
 
+    const handleClick = (id) => {
+        console.log('Diste Click...', id)
+    }
+
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
@@ -14,6 +18,7 @@ export default function Guitar({guitar}) {
                 <button 
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => handleClick(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
