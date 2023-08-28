c1add37d4006f46450f7d903150f3723494557fc
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-28 11:40:40 -0600

    29

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
 
+  const MAX_ITEMS = 5
+
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
     if(itemExists >= 0) { // existe en el carrito
@@ -24,11 +26,26 @@ function App() {
     setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
   }
 
+  function increaseQuantity(id) {
+    const updatedCart = cart.map( item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+
   return (
     <>
     <Header 
       cart={cart}
       removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
     />
     
     <main className="container-xl mt-5">
