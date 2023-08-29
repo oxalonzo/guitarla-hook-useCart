97ff3eaea977265fa34abeaf7fb9b521fd395eb0
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-28 18:28:10 -0600

    32

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { db } from './data/db'
@@ -10,6 +10,10 @@ function App() {
 
   const MIN_ITEMS = 1
   const MAX_ITEMS = 5
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
@@ -55,10 +59,8 @@ function App() {
   }
 
   function clearCart(e) {
-    e.preventDefault()
     setCart([])
   }
-
 
   return (
     <>
