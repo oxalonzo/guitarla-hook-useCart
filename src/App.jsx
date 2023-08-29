0946dca3e46ba8d78c360ddcd2c0b75fe6880802
Juan <codigoconjuan@MacBook-Pro-de-Juan.local>
2023-08-28 18:41:10 -0600

    33

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import { db } from './data/db'
 
 function App() {
 
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(initialCart)
 
   const MIN_ITEMS = 1
   const MAX_ITEMS = 5
